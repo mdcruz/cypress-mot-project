3923e84e8ec846ef78b25a89f17573c8aff44e2d
mdcruz <marie.cruz1@news.co.uk>
2020-08-18 23:02:03 +0100

    first cypress test

--- cypress/integration/todoapp.spec.js
+++ cypress/integration/todoapp.spec.js
@@ -2,6 +2,10 @@
 
 describe('Todo application', () => {
   it('should add a new todo item', () => {
-    /// test here
+    cy.visit('http://todomvc.com/examples/react/#/active');
+    cy.get('.new-todo').type('new todo {enter}');
+    cy.get('.todo-list li')
+      .should('have.length', 2)
+      .and('have.text', 'new todo');
   });
 });
