382c44deb17041416963dda0237c5f36dcb82948
mdcruz <marie.cruz1@news.co.uk>
2020-09-05 19:09:27 +0100

    limit login scenario to UI based for now

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,47 +1,27 @@
 /// <reference types="Cypress" />
 
 describe('Login feature', () => {
-  beforeEach(() => cy.fixture('login-details').as('testData'));
-
-  describe('Login via UI', () => {
-    beforeEach(() =>
-      cy.visit(`${Cypress.env('REST_BOOKER_PLATFORM')}/#/admin`)
-    );
-
-    it('should login successfully when given the correct credentials', () => {
-      cy.get('@testData').then((data) => {
-        cy.loginViaUI(
-          data.validCredentials.username,
-          data.validCredentials.password
-        );
-      });
-
-      cy.get('#frontPageLink').should('be.visible');
-    });
-
-    it('should fail login when given incorrect credentials', () => {
-      cy.get('@testData').then((data) => {
-        cy.loginViaUI(
-          data.invalidCredentials.username,
-          data.invalidCredentials.password
-        );
-      });
-
-      cy.get('#frontPageLink').should('not.be.visible');
-    });
+  beforeEach(() => {
+    cy.fixture('login-details').as('testData');
+    cy.visit(`${Cypress.env('REST_BOOKER_PLATFORM')}/#/admin`);
   });
 
-  describe('Login via API', () => {
-    it('should authorise the user successfully', () => {
-      cy.get('@testData').then((data) => {
-        cy.loginViaAPI(
-          data.validCredentials.username,
-          data.validCredentials.password
-        );
-      });
-
-      cy.visit(`${Cypress.env('REST_BOOKER_PLATFORM')}/#/admin`);
-      cy.get('#frontPageLink').should('be.visible');
+  it('should login successfully when given the correct credentials', () => {
+    cy.get('@testData').then((data) => {
+      cy.login(data.validCredentials.username, data.validCredentials.password);
     });
+
+    cy.get('#frontPageLink').should('be.visible');
+  });
+
+  it('should fail login when given incorrect credentials', () => {
+    cy.get('@testData').then((data) => {
+      cy.login(
+        data.invalidCredentials.username,
+        data.invalidCredentials.password
+      );
+    });
+
+    cy.get('#frontPageLink').should('not.be.visible');
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,16 +1,5 @@
-Cypress.Commands.add('loginViaUI', (username, password) => {
+Cypress.Commands.add('login', (username, password) => {
   cy.get('#username').type(username);
   cy.get('#password').type(password);
   cy.get('[data-testid="submit"]').click();
-});
-
-Cypress.Commands.add('loginViaAPI', (username, password) => {
-  cy.request({
-    method: 'POST',
-    url: `${Cypress.env('REST_BOOKER_PLATFORM')}/auth/login`,
-    body: {
-      username,
-      password,
-    },
-  }).then((res) => cy.setCookie('token', res.body.token));
 });
